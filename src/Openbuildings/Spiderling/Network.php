587dd94e6072745aa2b3ec272bec9b214d73d92b
Ivan Kerin <ivan@openbuildings.com>
2013-07-20 14:28:51 +0300

    add some docs for network class

--- src/Openbuildings/Spiderling/Network.php
+++ src/Openbuildings/Spiderling/Network.php
@@ -3,6 +3,8 @@
 namespace Openbuildings\Spiderling;
 
 /**
+ * Network helper for deailng with ports
+ *
  * @package    Func_Test
  * @author     Ivan Kerin
  * @copyright  (c) 2012 OpenBuildings Ltd.
@@ -10,6 +12,12 @@ namespace Openbuildings\Spiderling;
  */
 class Network {
 
+	/**
+	 * Check if a port is open
+	 * @param  string  $host
+	 * @param  integer  $port
+	 * @return boolean
+	 */
 	public static function is_port_open($host, $port)
 	{
 		$connection = @fsockopen($host, $port);
@@ -21,6 +29,16 @@ class Network {
 		return TRUE;
 	}
 
+	/**
+	 * Find an open port in a given range, trying several times.
+	 * Return FALSE if no open port is found after a timeout (1 second by default)
+	 * 
+	 * @param  string  $host
+	 * @param  integer $range_start
+	 * @param  integer $range_end
+	 * @param  integer $timeout
+	 * @return integer|boolean
+	 */
 	public static function ephimeral_port($host, $range_start = 1000, $range_end = 5000, $timeout = 1000)
 	{
 		return Attempt::make(function() use ($host, $range_start, $range_end) {
